a6d7777352ba4384a75158958ae63f373d093494
micolse <micol.serri@edu.unifi.it>
2026-08-10 12:59:13 +0200

    Implementa interfaccia Prof (creazione/pubblicazione tesi) e Studente (catalogo)

--- src/main/java/business/GestioneTesiController.java
+++ src/main/java/business/GestioneTesiController.java
@@ -7,4 +7,8 @@ public interface GestioneTesiController {
     List<Tesi> getTesiDisponibili();
     boolean creaTesi(Tesi tesi);
     boolean modificaTesi(Tesi tesi);
+
+    boolean pubblicaTesi(Tesi tesi);
+    List<Tesi> cercaTesi(String keyword);
+    List<Tesi> getTesiByProfessore(int idProfessore);
 }
